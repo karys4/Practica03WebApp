840b21ad8cb698d4688683ce11e62176881d45c5
karys4 <kary.github@gmail.com>
2019-02-13 11:43:33 -0600

    Agregar archivos de proyecto.

---
+++ Practica03WebApp/Pages/Ejemplo.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practica03WebApp.Pages
+{
+    public class EjemploModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
